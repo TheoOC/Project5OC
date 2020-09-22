307900ad9aec54a89053700bb4fd487552d0f974
TheoOC <61147292+TheoOC@users.noreply.github.com>
2020-09-22 21:37:30 +0200

    confirmation price is now in euros instead on cents

--- javascript/confirmation.js
+++ javascript/confirmation.js
@@ -1,5 +1,8 @@
+console.log(JSON.stringify(localStorage.orderId));
+
 let orderId = document.getElementById("orderId");
 let prixTotal = document.getElementById("prixTotal");
+
 orderId.insertAdjacentHTML("afterbegin", JSON.parse(localStorage.orderId));
 prixTotal.insertAdjacentHTML("afterbegin", getPrixTotal());
 
@@ -9,8 +12,9 @@ function getPrixTotal() {
     console.log(items);
     for (let i = 0; i < items.length; ++i) {
         for (let j = 0; j < items[i].count; ++j) {
-            temp += items[i].price;
+            temp += items[i].price / 100;
         }
     }
+    temp += ' €';
     return temp;
 }
